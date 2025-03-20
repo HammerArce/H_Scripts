04cae12da70b6c396f285d6d1807340e5c10bb0d
Hammer Santamaria <arce.hammer@gmail.com>
2025-03-20 10:12:24 -0500

    first commit

---
+++ SQL server commands.sql
@@ -0,0 +1,15 @@
+
+
+-------Data Definition Lenguage
+create database nombreBasedeDatos;
+
+drop database nombreBasedeDatos;
+
+alter database nombreBasedeDatos modify name = nuevoNombre;
+
+create table nombreTabla (
+primerCampo varchar(20)
+
+-------Data Query Lenguage
+select * from sys.databases;
+
